be1fab8e7a270dc6733cb0c3ca5899c1706bf9b6
Anna Beatriz Lucena Lira <anna.lira@ccc.ufcg.edu.br>
2022-02-21 20:56:11 -0300

    inicia implementação da tela principal

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,49 @@
-module Main where
+printEspaco :: IO()
+printEspaco = do
+    putStr "\n"
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+telaInicial :: IO()
+telaInicial = do
+    printEspaco
+    putStr "--------------------------BET' O Carrero--------------------------\n"
+    printEspaco
+    putStr "Casa de recreação que oferece jogos digitais operados por diversão"
+    printEspaco
+
+getMoeda :: IO()
+getMoeda = do
+    putStr "                      $$$$$$$$$$$$$$$$$$\n"
+    putStr "                     $$$$$$$$$$$$$$$$$$$$\n"
+    putStr "                   $$$$   ==||=||==    $$$$\n"
+    putStr "                   $$$$  || || ||      $$$$\n"
+    putStr "                   $$$$  || || ||      $$$$\n"
+    putStr "                   $$$$  || || ||      $$$$\n"
+    putStr "                   $$$$   ==||======   $$$$\n"
+    putStr "                   $$$$     || ||  ||  $$$$\n"
+    putStr "                   $$$$     || ||  ||  $$$$\n"
+    putStr "                   $$$$     || ||  ||  $$$$\n"
+    putStr "                   $$$$  ===||=||===   $$$$\n"
+    putStr "                     $$$$$$$$$$$$$$$$$$$$\n"
+    putStr "                      $$$$$$$$$$$$$$$$$$\n"
+    printEspaco
+    
+
+
+getOpcoesMenu :: IO()
+getOpcoesMenu = do
+    putStr "Selecione a opção:\n"
+    printEspaco
+    putStr "1. Listagem de Jogos\n"
+    putStr "2. Instruções\n"
+    putStr "3. Ranking dos Jogadores\n"
+    putStr "4. Créditos\n"
+    putStr "5. Sair\n"
+
+--listaJogos
+
+main :: IO()
+main = do
+    telaInicial
+    printEspaco
+    getMoeda
+    getOpcoesMenu

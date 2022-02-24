873d9443b2dd3834f30186c949a51ecb52b34501
Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2022-02-23 21:10:46 -0300

    Adding title in ASCII

--- src/Main.hs
+++ src/Main.hs
@@ -1,49 +1,53 @@
-printEspaco :: IO()
+printEspaco :: IO ()
 printEspaco = do
-    putStr "\n"
+  putStr "\n"
 
-telaInicial :: IO()
+telaInicial :: IO ()
 telaInicial = do
-    printEspaco
-    putStr "--------------------------BET' O Carrero--------------------------\n"
-    printEspaco
-    putStr "Casa de recreação que oferece jogos digitais operados por diversão"
-    printEspaco
+  printEspaco
+  putStrLn "__________        __ /\\ ________    _________                                           "
+  putStrLn "\\______   \\ _____/  |)/ \\_____  \\   \\_   ___ \\_____ ______________   ___________  ____  "
+  putStrLn " |    |  _// __ \\   __\\  /   |   \\  /    \\  \\/\\__  \\\\_  __ \\_  __ \\_/ __ \\_  __ \\/  _ \\ "
+  putStrLn " |    |   \\  ___/|  |   /    |    \\ \\     \\____/ __ \\|  | \\/|  | \\/\\  ___/|  | \\(  <_> )"
+  putStrLn " |______  /\\___  >__|   \\_______  /  \\______  (____  /__|   |__|    \\___  >__|   \\____/ "
+  putStrLn "        \\/     \\/               \\/          \\/     \\/                   \\/              "
 
-getMoeda :: IO()
+  printEspaco
+  putStr "Casa de recreação que oferece jogos digitais operados por diversão"
+  printEspaco
+
+getMoeda :: IO ()
 getMoeda = do
-    putStr "                      $$$$$$$$$$$$$$$$$$\n"
-    putStr "                     $$$$$$$$$$$$$$$$$$$$\n"
-    putStr "                   $$$$   ==||=||==    $$$$\n"
-    putStr "                   $$$$  || || ||      $$$$\n"
-    putStr "                   $$$$  || || ||      $$$$\n"
-    putStr "                   $$$$  || || ||      $$$$\n"
-    putStr "                   $$$$   ==||======   $$$$\n"
-    putStr "                   $$$$     || ||  ||  $$$$\n"
-    putStr "                   $$$$     || ||  ||  $$$$\n"
-    putStr "                   $$$$     || ||  ||  $$$$\n"
-    putStr "                   $$$$  ===||=||===   $$$$\n"
-    putStr "                     $$$$$$$$$$$$$$$$$$$$\n"
-    putStr "                      $$$$$$$$$$$$$$$$$$\n"
-    printEspaco
-    
+  putStr "                      $$$$$$$$$$$$$$$$$$\n"
+  putStr "                     $$$$$$$$$$$$$$$$$$$$\n"
+  putStr "                   $$$$   ==||=||==    $$$$\n"
+  putStr "                   $$$$  || || ||      $$$$\n"
+  putStr "                   $$$$  || || ||      $$$$\n"
+  putStr "                   $$$$  || || ||      $$$$\n"
+  putStr "                   $$$$   ==||======   $$$$\n"
+  putStr "                   $$$$     || ||  ||  $$$$\n"
+  putStr "                   $$$$     || ||  ||  $$$$\n"
+  putStr "                   $$$$     || ||  ||  $$$$\n"
+  putStr "                   $$$$  ===||=||===   $$$$\n"
+  putStr "                     $$$$$$$$$$$$$$$$$$$$\n"
+  putStr "                      $$$$$$$$$$$$$$$$$$\n"
+  printEspaco
 
-
-getOpcoesMenu :: IO()
+getOpcoesMenu :: IO ()
 getOpcoesMenu = do
-    putStr "Selecione a opção:\n"
-    printEspaco
-    putStr "1. Listagem de Jogos\n"
-    putStr "2. Instruções\n"
-    putStr "3. Ranking dos Jogadores\n"
-    putStr "4. Créditos\n"
-    putStr "5. Sair\n"
+  putStr "Selecione a opção:\n"
+  printEspaco
+  putStr "1. Listagem de Jogos\n"
+  putStr "2. Instruções\n"
+  putStr "3. Ranking dos Jogadores\n"
+  putStr "4. Créditos\n"
+  putStr "5. Sair\n"
 
 --listaJogos
 
-main :: IO()
+main :: IO ()
 main = do
-    telaInicial
-    printEspaco
-    getMoeda
-    getOpcoesMenu
+  telaInicial
+  printEspaco
+  getMoeda
+  getOpcoesMenu

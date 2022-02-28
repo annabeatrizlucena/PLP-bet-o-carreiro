5fc204b4c33dd54b0052bb70936204501735031b
Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2022-02-28 11:45:59 -0300

    Fix Data  Methods: Phases Related

--- src/Ourdb/Db.hs
+++ src/Ourdb/Db.hs
@@ -1,9 +1,7 @@
-module Database.DB (
-    insertPhase
-) where
-
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedStrings #-}
+
+module Ourdb.Db where
 
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.FromRow
@@ -11,8 +9,13 @@ import GHC.Int
 
 data User = User {username :: String, score :: Int} deriving (Show, Read, Eq)
 
+data Phase = Phase {sequencia :: String} deriving (Show, Read, Eq)
+
 instance FromRow User where
   fromRow = User <$> field <*> field
+
+instance FromRow Phase where
+  fromRow = Phase <$> field
 
 getUsers :: Connection -> IO [User]
 getUsers conn = do
@@ -27,18 +30,22 @@ insertUsernameAndUserScore conn username score = do
   execute conn "INSERT INTO users (username, score) VALUES (?, ?)" (username, score)
   print username
 
-insertPhase :: Connection -> Int -> String -> String -> IO()
-savePhase conn id sequencia nivel = do
-  execute conn "INSERT INTO phases (id, sequencia, nivel) VALUES (?, ?)" (id, sequencia,nivel )
+insertPhase :: Connection -> Int -> String -> String -> IO ()
+insertPhase conn id sequencia nivel = do
+  execute conn "INSERT INTO phases (id, sequencia, nivel) VALUES (?, ?, ?)" (id, sequencia, nivel)
+  print id
 
-getPhase :: Connection -> String -> Int-> String
-getPhase conn fase id_phase = do
-  query  conn "SELECT id FROM phases WHERE id == (?)" (id_phase)
+getPhase :: Connection -> IO [Phase]
+getPhase conn = do
+  query_ conn "SELECT phases.sequencia FROM phases WHERE id = (1)" :: IO [Phase]
 
-main :: IO ()
-main = do
+connectOurDatabase :: IO ()
+connectOurDatabase = do
   conn <- connectPostgreSQL "host='ec2-52-204-196-4.compute-1.amazonaws.com' port=5432 dbname='dchdcr7iap07pl' user='alxxpufsycowxx' password='ff89341db8b88bd30ae01f43290cbe396c7e3340fba82ebb52915b6a0f560998'"
   execute_ conn "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username VARCHAR(50) NOT NULL, score INTEGER NOT NULL)"
-  execute_ conn "CREATE TABLE IF NOT EXISTS phases (id INTEGER PRIMARY KEY, sequencia VARCHAR(10) NOT NULL)"
-
+  execute_ conn "CREATE TABLE IF NOT EXISTS phases (id INTEGER PRIMARY KEY, sequencia VARCHAR(10) NOT NULL, nivel VARCHAR(10) NOT NULL)"
   print "Connected to database"
+
+insertAllPhases :: Connection -> IO ()
+insertAllPhases conn = do
+  insertPhase conn 1 "aaswd" "facil"

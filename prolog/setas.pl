b0453687e6929572a73ad62e946614202ec46315
Natalia Salvino <nataliasalvino86@gmail.com>
2022-03-30 22:01:28 -0300

    main

--- prolog/setas.pl
+++ prolog/setas.pl
@@ -53,12 +53,12 @@ compareSequence :-
     read(Y),
     validateSequence(X,Y).
 
-getUserInput(Time,X) :-
+getUserInput(Time,Y) :-
     writeln("\nDigite sua resposta:\n"),
     current_input(I),
     wait_for_input([I], [Input], Time),
     read_string(Input, _, String),
-    X = String; X = "\nO seu tempo acabou :(".
+    Y = String; Y = "\nO seu tempo acabou :(".
 
 cls :- write('\33\[2J').
 
@@ -70,5 +70,5 @@ main :-
     initArrowsGame,
     read(Input),
     getOption(Input, X),
-    getUserInput(5,X).
+    getUserInput(5,Y).
     
